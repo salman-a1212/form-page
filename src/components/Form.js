4b0eaf072ce9bc654b42fc9a11b824116372aa32
Salman Aslam <salman62893@gmail.com>
2023-01-21 12:08:59 +0500

    push to git

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div className='ms-5 me-5'><hr />
+            <div className="content ms-5 me-5">
+
+                <h2 className='text-center'>ACH Vendor Payment Authorization Agreement
+                </h2>
+                <p className='text-start'>Company Name: The Company Corp</p>
+                <p className='text-start'>A/R Contact Name: Jane Fox</p>
+                <p className='text-start'>A/R Contact Phone: +1 344 288 6980</p>
+                <p className='text-start'>A/R Contact Email: ar@gmail.com</p>
+                <br />
+                <p className='text-start'>Bank Name: Bank of America</p>
+                <p className='text-start'>Routing No: 7363765</p>
+                <p className='text-start'>Account No: 28375436777</p>
+                <p className='text-start'>Account Full Name: The Company Corp</p>
+
+
+                <br />
+                <p>I hereby authorize The company corp, Inc. to initiate entries to my checking/savings accounts at the financial institution listed above, and, if necessary, initiate adjustments for any transactions credited/debited in error. This authority will remain in effect until Branded Group, Inc. is notified by writing to cancel or changes.</p>
+
+                <br />
+                <p>n addition, please use the link below to securely enter your bank information & Email vendors@jhhhh.com with a Signed copy of this ACH Vendor Payment Authorization Agreement to confirm completion.
+
+                    <br />
+                    Kindly click on the link to enter your bank details: <a href='/'>https://forms.gle/R7dcuTzMoi6543yfW736338</a></p>
+
+
+
+
+            </div>
+
+
+
+        </div>
+
+    )
+}
+
+export default Form
